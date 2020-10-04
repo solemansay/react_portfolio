476dce6b92226b7c7f409566c71e2bc38ea9307c
SoloJette <solemansay@gmail.com>
2020-10-03 20:10:35 -0700

    Updates

---
+++ precache-manifest.2aac0d17f35df211b3ba090a010e7e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba3bc46b1a66cf1564e231d440e70a2",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "64e37194829f0390b2b7",
+    "url": "/react_portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "86fd9f7fb3c4ea25a6a5",
+    "url": "/react_portfolio/static/css/main.1ae5e712.chunk.css"
+  },
+  {
+    "revision": "64e37194829f0390b2b7",
+    "url": "/react_portfolio/static/js/2.4083aca2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_portfolio/static/js/2.4083aca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fd9f7fb3c4ea25a6a5",
+    "url": "/react_portfolio/static/js/main.5fa93cd7.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  }
+]);
